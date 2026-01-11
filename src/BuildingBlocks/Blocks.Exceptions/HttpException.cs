0da45e70f01334fa2dc308167d6068d8b7634591
ldumit <dumitrescu.laurentiu@gmail.com>
2026-01-11 23:35:41 +0100

    [Auth] CreateUser - SendConfirmationEmail

--- src/BuildingBlocks/Blocks.Exceptions/HttpException.cs
+++ src/BuildingBlocks/Blocks.Exceptions/HttpException.cs
@@ -4,19 +4,19 @@ namespace Blocks.Exceptions;
 
 public class HttpException : Exception
 {
-		public HttpException(HttpStatusCode statusCode, string? message, Exception ex)
-				: base(message, ex)
-		{
-				this.HttpStatusCode = statusCode;
-		}
+	public HttpException(HttpStatusCode statusCode, string? message, Exception ex)
+		: base(message, ex)
+	{
+		this.HttpStatusCode = statusCode;
+	}
 
-		public HttpException(HttpStatusCode statusCode, string? message)
-				: base(string.IsNullOrEmpty(message) ? statusCode.ToString() : message)
-		{
-				this.HttpStatusCode = statusCode;
-		}
+	public HttpException(HttpStatusCode statusCode, string? message)
+		: base(string.IsNullOrEmpty(message) ? statusCode.ToString() : message)
+	{
+		this.HttpStatusCode = statusCode;
+	}
 
-		public HttpStatusCode HttpStatusCode { get; }
+	public HttpStatusCode HttpStatusCode { get; }
 
-		public int StatusCode => (int)HttpStatusCode;
+	public int StatusCode => (int)HttpStatusCode;
 }
--- src/Services/Auth/Auth.API/Features/CreateUser/SendConfirmationEmailOnUserCreatedHandler.cs
+++ src/Services/Auth/Auth.API/Features/CreateUser/SendConfirmationEmailOnUserCreatedHandler.cs
@@ -1,12 +1,38 @@
-﻿using FastEndpoints;
+﻿using Microsoft.Extensions.Options;
+using Flurl;
+using Blocks.AspNetCore;
+using EmailService.Contracts;
 using Auth.Domain.Users.Events;
+using Auth.Domain.Users;
+using FastEndpoints;
 
 namespace Auth.API.Features.CreateUser;
 
 public class SendConfirmationEmailOnUserCreatedHandler
+    (IEmailService emailService, IHttpContextAccessor httpContextAccessor, IOptions<EmailOptions> emailOptions)
     : IEventHandler<UserCreated>
 {
     public async Task HandleAsync(UserCreated eventModel, CancellationToken ct)
     {
+        var url = httpContextAccessor.HttpContext?.Request.BaseUrl()
+            .AppendPathSegment("set-first-password")
+            .SetQueryParams(new { eventModel.RessetPasswordToken });
+
+        var emailMessage = BuildConfirmationEmail(eventModel.User, url, emailOptions.Value.EmailFromAddress);
+
+        await emailService.SendEmailAsync(emailMessage);
+    }
+
+    public EmailMessage BuildConfirmationEmail(User user, string resetLink, string fromEmailAddress)
+    {
+        const string ConfirmationEmail =
+            "Dear {0},<br/>An account has been created for you.<br/>Please set your password using the following URL: <br/>{1}";
+
+        return new EmailMessage(
+            "Your Account Has Been Created – Set Your Password",
+            new Content(ContentType.Html, string.Format(ConfirmationEmail, user.FullName, resetLink)),
+            new EmailAddress("articles", fromEmailAddress),
+            new List<EmailAddress> { new EmailAddress(user.FullName, user.Email!) }
+            );
     }
 }
