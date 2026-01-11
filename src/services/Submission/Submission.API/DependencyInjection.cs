de8e954fe5a1aacd33e5e9443819cfd07bd7cfc6
ldumit <dumitrescu.laurentiu@gmail.com>
2026-01-11 09:58:41 +0100

    [Submission] UploadManuscriptFile - FileStorage Module

--- src/services/Submission/Submission.API/DependencyInjection.cs
+++ src/services/Submission/Submission.API/DependencyInjection.cs
@@ -1,15 +1,20 @@
-﻿namespace Submission.API;
+﻿using FileStorage.MongoGridFS;
+
+namespace Submission.API;
 
 public static class DependencyInjection
 {
-		public static IServiceCollection AddAPIServices(this IServiceCollection services, IConfiguration configuration)
-		{
-				services
-						.AddMemoryCache()
-						.AddEndpointsApiExplorer()
-						.AddSwaggerGen()
-						;
-				// Add API specific services here
-				return services;
-		}
+	public static IServiceCollection AddAPIServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		services
+			.AddMemoryCache()
+			.AddEndpointsApiExplorer()
+			.AddSwaggerGen()
+			;
+		// Add API specific services here
+
+		services.AddMongoFileStorage(configuration);
+
+		return services;
+	}
 }
