1c2afaef492e2b8ef52e0073b392eaadd6545fdf
ldumit <dumitrescu.laurentiu@gmail.com>
2026-01-16 15:58:53 +0100

    [Auth] DependencyInjection

---
+++ src/BuildingBlocks/Articles.Security/ConfigureAuthentication.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Articles.Security;
+
+public static class ConfigureAuthentication
+{
+    public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(jwtOptions =>
+            {
+                // todo - configure JwtOptions
+            });
+
+        return services;
+    }
+}
---
+++ src/Services/Auth/Auth.API/DependencyInjection.cs
@@ -0,0 +1,63 @@
+﻿using Auth.Domain.Users;
+using Auth.Persistence;
+using EmailService.Smtp;
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using Articles.Security;
+
+namespace Auth.API;
+
+public static class DependenciesConfiguration
+{
+    public static IServiceCollection ConfigureApiOptions(this IServiceCollection services, IConfiguration config)
+    {
+        //use it for configuring options
+        services
+            .AddAndValidateOptions<JwtOptions>(config);
+
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration config)
+    {
+        services
+            .AddFastEndpoints()
+            .SwaggerDocument()
+            .AddEndpointsApiExplorer()
+            .AddSwaggerGen()
+            .AddJwtAuthentication(config)
+            .AddJwtIdentity(config)
+            .AddAuthorization();
+
+        services.AddSmtpEmailService(config);
+
+        return services;
+    }
+
+    public static IServiceCollection AddJwtIdentity(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddIdentityCore<User>(options =>
+        {
+            // Lockout settings
+            options.Lockout.AllowedForNewUsers = true;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+
+            //options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+/ ";
+        })
+        .AddRoles<Auth.Domain.Roles.Role>()
+        .AddEntityFrameworkStores<AuthDbContext>()
+        .AddSignInManager<SignInManager<User>>()
+        .AddDefaultTokenProviders();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.ClaimsIdentity.RoleClaimType = ClaimTypes.Role;
+        });
+
+        return services;
+    }
+}
